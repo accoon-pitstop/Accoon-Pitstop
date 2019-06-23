a34fa106afa61f47aca5f1d13971a40412d6e6bb
chathurangasandun <csk@tiqri.com>
2019-06-23 22:09:33 +0300

    add get clone all

---
+++ scripts/git-clone-all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+REPOSITORIES=(Pitstop-CustomerManagement )
+
+if [ "$1" = "-p" ]
+  then
+    echo ${REPOSITORIES[@]} | sed -E -e 's/[[:blank:]]+/\n/g' | xargs -I {} -n 1 -P 0 sh -c 'printf "========================================================\nCloning repository: {}\n========================================================\n"; git clone https://github.com/devmentors/{}.git'
+  else
+    for REPOSITORY in ${REPOSITORIES[*]}
+    do
+      echo ========================================================
+      echo Cloning repository: $REPOSITORY
+      echo ========================================================
+	  cd ../../
+	  mkdir $REPOSITORY
+	  cd $REPOSITORY
+      REPO_URL=https://github.com/accoon-pitstop/$REPOSITORY.git
+      git clone $REPO_URL
+    done
+fi
+
