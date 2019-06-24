cf970b735937a4f9ce0d534a30efed6aedf38c7d
chathurangasandun <csk@tiqri.com>
2019-06-24 07:14:15 +0300

    change shell

--- scripts/git-clone-all.sh
+++ scripts/git-clone-all.sh
@@ -3,7 +3,7 @@ REPOSITORIES=(Pitstop-CustomerManagement )
 
 if [ "$1" = "-p" ]
   then
-    echo ${REPOSITORIES[@]} | sed -E -e 's/[[:blank:]]+/\n/g' | xargs -I {} -n 1 -P 0 sh -c 'printf "========================================================\nCloning repository: {}\n========================================================\n"; git clone https://github.com/devmentors/{}.git'
+    echo ${REPOSITORIES[@]} | sed -E -e 's/[[:blank:]]+/\n/g' | xargs -I {} -n 1 -P 0 sh -c 'printf "========================================================\nCloning repository: {}\n========================================================\n"; git clone https://github.com/accoon-pitstop/{}.git'
   else
     for REPOSITORY in ${REPOSITORIES[*]}
     do
